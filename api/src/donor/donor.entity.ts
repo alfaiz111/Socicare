129671e3b8918c69bff8cff1a3c380faf0bc1326
alfaiz111 <muhammadarif120419@gmail.com>
2026-06-05 21:20:20 +0700

    api

---
+++ api/src/donor/donor.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Donor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nama: string;
+
+  @Column()
+  email: string;
+
+  @Column('decimal', { default: 0 })
+  jumlah_donasi: number;
+
+  @Column('decimal', { default: 0 })
+  total_donasi: number;
+
+  @Column()
+  status: string;
+}
