d02201fd504e565fbfac57c2be0037874624ef4d
alfaiz111 <muhammadarif120419@gmail.com>
2026-06-05 21:12:11 +0700

    api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Campaign } from './campaign/campaign.entity';
+import { Disaster } from './disaster/disaster.entity';
+import { Donor } from './donor/donor.entity';
+import { DonationVerification } from './donation-verification/donation-verification.entity';
+import { Distribution } from './distribution/distribution.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'donasi_app',
+      entities: [Campaign, Disaster, Donor, DonationVerification, Distribution],
+      synchronize: true, // DEV ONLY
+    }),
+  ],
 })
 export class AppModule {}
