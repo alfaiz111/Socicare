546417a5ed1c813dce637a1418bc374e97aa23cb
Fadly Mustofainal Ahyar <fadlymustofa0@gmail.com>
2026-06-05 21:22:16 +0700

    fixed halaman penyaluran donasi

--- frontend-cms/src/app/penyaluran/page.tsx
+++ frontend-cms/src/app/penyaluran/page.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
 
-import { SidebarProvider } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/layout/sidebar"
-import TopBar from "@/components/layout/topbar"
-import DistributionTable from "@/components/penyaluran/distribution-table"
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/layout/sidebar";
+import TopBar from "@/components/layout/topbar";
+import DistributionTable from "@/components/penyaluran/distribution-table";
 
 export default function PenyaluranPage() {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen bg-[#f5f5f7]">
+      <div className="flex min-h-screen w-full bg-[#f5f5f7]">
         <AppSidebar />
 
-        <div className="flex-1 flex flex-col">
+        <SidebarInset className="flex-1">
           <TopBar />
 
           <main className="p-6">
             <DistributionTable />
           </main>
-        </div>
+        </SidebarInset>
       </div>
     </SidebarProvider>
-  )
+  );
 }
