956c66ff79ebce01e26028d0833fb3260a83170b
Fadly Mustofainal Ahyar <fadlymustofa0@gmail.com>
2026-06-08 12:14:15 +0700

    merapihkan barisan code

--- api/src/campaign/dto/update-campaign.dto.ts
+++ api/src/campaign/dto/update-campaign.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCampaignDto } from './create-campaign.dto';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export class UpdateCampaignDto extends PartialType(CreateCampaignDto) {}
