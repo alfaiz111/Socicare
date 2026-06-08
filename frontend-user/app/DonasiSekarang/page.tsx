562bcec7f64696d87c4a392392aaf539e0229cbb
alfaiz111 <muhammadarif120419@gmail.com>
2026-06-08 21:02:12 +0700

    perbaikan eror parameter name

--- frontend-user/app/DonasiSekarang/page.tsx
+++ frontend-user/app/DonasiSekarang/page.tsx
@@ -221,7 +221,7 @@ export default function DonasiSekarang() {
 }
 
 // IMAGE
-const getImage = (name) => {
+const getImage = (name: string) => {
   switch (name) {
     case "Banjir":
       return require("../../assets/images/banjir.jpg");
