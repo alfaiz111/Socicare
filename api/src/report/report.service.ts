3879e50af033c584818c8b87e6712e80fc8c3280
Fadly Mustofainal Ahyar <fadlymustofa0@gmail.com>
2026-06-08 12:24:39 +0700

    membuat report.service

--- api/src/report/report.service.ts
+++ api/src/report/report.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
+
+  async getSummary() {
+    const totalDonasi =
+      await this.prisma.donation.aggregate({
+        _sum: {
+          amount: true,
+        },
+      });
+
+    const totalPenyaluran =
+      await this.prisma.distribution.aggregate({
+        _sum: {
+          amount: true,
+        },
+      });
+
+    const jumlahDonatur =
+      await this.prisma.donor.count();
+
+    return {
+      periode: 'Semua Data',
+      totalDonasi:
+        totalDonasi._sum.amount || 0,
+      totalPenyaluran:
+        totalPenyaluran._sum.amount || 0,
+      jumlahDonatur,
+    };
+  }
+}
